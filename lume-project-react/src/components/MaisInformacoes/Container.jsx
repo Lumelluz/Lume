52914696f5844c4c14532a3d5141374d7d003a0d
Guilherme Marcilio Viana Silva <guizexrlq@gmail.com>
2025-05-17 15:14:03 -0300

    Mais informações em desenvolvimento

---
+++ lume-project-react/src/components/MaisInformacoes/Container.jsx
@@ -0,0 +1,15 @@
+import maisInfo1 from '../../assets/img/maisInfo1.png';
+
+function Container() {
+    return(
+        <div>
+        <img src={maisInfo1} alt="Imagem de um entregador" />
+        <h2>Não se<br></br> preocupe com <br></br> entregas</h2>
+        <p>A Lume já oferece serviço de <br></br>entrega parar seus clientes.<br></br>
+            Assim sua empresa não<br></br> precisa se preocupar!</p>
+    </div>
+    )
+    
+    }
+
+export default Container
