a5c20d18726fe6394a22ce4226c2042a821b28c5
Murilo Macedo Silva <152343609+MuriloMacedoSilva@users.noreply.github.com>
2025-05-17 12:40:17 -0300

    subi o código para debugar
    subindo código para debugar pois não esta aparecendo conteudo

---
+++ lume-project-react/src/components/ApresentacaoSobreNos/index.jsx
@@ -0,0 +1,14 @@
+import styles from '../ApresentacaoSobreNos/ApresentacaoSobreNos.modules.css'
+
+function ApresentacaoSobreNos() {
+    return (
+        <section className={styles.sectionQueAgrupaContainer}>
+            <h2>Sobre Nós</h2>
+            <p>• Somos uma equipe formada por sete jovens talentos em tecnologia, unidos pelo Instituto PROA  com o propósito de criar soluções que gerem impacto positivo na sociedade.</p>
+            <p>• Nosso time é diverso e completo.</p>
+            <p>• Somos uma equipe formada por sete jovens talentos em tecnologia, unidos pelo Instituto PROA  com o propósito de criar soluções que gerem impacto positivo na sociedade.</p>
+        </section>
+    )
+}
+
+export default ApresentacaoSobreNos;
--- lume-project-react/src/routes.jsx
+++ lume-project-react/src/routes.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import PageBase from "./pages/PageBase";
 import Home from "./pages/Home";
 import TelaLogin from "./pages/TelaLogin";
+import SobreNos from "./pages/SobreNos";
 
 function AppRoutes() {
     return (
@@ -10,6 +11,7 @@ function AppRoutes() {
                 <Route path="/" element={<PageBase />}>
                     <Route path="/" element={<Home />} />
                     <Route path="/login" element={<TelaLogin />} />
+                    <Route path="/SobreNos" element={<SobreNos />} />
 
                 </Route>
             </Routes>
