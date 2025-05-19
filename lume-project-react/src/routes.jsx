aa7373daed7a7a57fb39f5ca21180b7260155758
liutilucas <lucasliuti90@gmail.com>
2025-05-19 03:25:57 -0300

    Tela login password

--- lume-project-react/src/routes.jsx
+++ lume-project-react/src/routes.jsx
@@ -2,14 +2,16 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import PageBase from "./pages/PageBase";
 import Home from "./pages/Home";
 import TelaLogin from "./pages/TelaLogin";
+import LoginSenha from "./pages/LoginSenha";
 
 function AppRoutes() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<PageBase />}>
-                    <Route path="/" element={<Home />} />
+                    <Route path="/" element={<Home />} />                   
                     <Route path="/login" element={<TelaLogin />} />
+                    <Route path="/loginpassword" element ={<LoginSenha />} />
 
                 </Route>
             </Routes>
