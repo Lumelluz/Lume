6e5e7db1ccd83dbbfe19b54cf50a1643cc0ad623
liutilucas <lucasliuti90@gmail.com>
2025-05-26 21:35:43 -0300

    checkout finalizando

--- lume-project-react/src/pages/CheckOutPagamento/index.jsx
+++ lume-project-react/src/pages/CheckOutPagamento/index.jsx
@@ -6,6 +6,7 @@ import remover from '../../assets/img/remover.png'
 import imgProduto from '../../assets/img/imgProduto.png'
 import buttonRemover from '../../assets/img/buttonRemover.png'
 import adicionarItems from '../../assets/img/adicionarItems.png'
+import endereçoMais from '../../assets/img/endereçoMais.png'
 
 
 
@@ -19,6 +20,7 @@ function CheckOutPagamento () {
                         <h2>Métodos de pagamento</h2>
                         </div>
                         <div className={styles.PagamentoPix}><span>Pagar com pix</span></div>
+                        <div className={styles.pagamentoBoleto}><span>Pagar com boleto</span></div>
                         <div className={styles.methodDebito}><span>Cartão de debito</span></div>
                         <div className={styles.containerPagament}>
                             <span className={styles.cardDebito}>Cartão de debito</span>
@@ -46,25 +48,46 @@ function CheckOutPagamento () {
                         </div>
                     </div>
                     <div className={styles.divisionTwo}>
-                        <div className={styles.backgoundProduto}>
-                            <h2>lista de itens</h2>
-                            <div>
-                                <img src={imgProduto} alt="imgproduto" /> <span>Kit utensilios 5 <br />peças Ecokitchen</span> <br /><span>R$23,56</span>
-                                <div className={styles.grupButton}>
-                                    <button className={styles.iconsAdd}>
-                                        <img src={remover} alt="" />
-                                    </button>         
-                                    <button>                        
-                                        <img src={adicionarItems} alt="" />
-                                    </button>  
+                        <div className={styles.secondColum}>
+                            <div className={styles.tester}>
+                                <h2 className={styles.itensProduto}>lista de itens</h2>
+                                <div className={styles.centralizadorProduto}></div>
+                                <div className={styles.backgoundProduto}> 
+                                    <img src={imgProduto} alt="imgproduto" />
+                                    <div className={styles.infoProduto}>
+                                        <span>Kit 5 utensilios <br/>
+                                            peças Ecokitchen</span> <span className={styles.preçoproduto}>R$23,56
+                                        </span>                                 
+                                    </div>
+                                    <div className={styles.junção}>
+                                        <div className={styles.grupButton}>
+                                            <button className={styles.iconsAdd}>
+                                                <img src={remover} alt="" />
+                                            </button>         
+                                            <button>                        
+                                                <img src={adicionarItems} alt="" />
+                                            </button>  
+                                        </div>
+                                    <span>Quantidade 1</span>    
+                                    </div>
                                 </div>
-                                <span>Quantidade 1</span>
-                            </div>       
+                                <div className={styles.produtoFinal}>
+                                    <div className={styles.divisionGroupOne}>
+                                        <h2>Detalhes de Pagamento</h2>
+                                        <div className={styles.divisionGroupTwo}></div>
+                                    </div>
+                                    <div className={styles.produtoPreço}>
+                                        <div className={styles.preçoTotal}>Preço total</div>
+                                    </div>
+                                        <div className={styles.endereço}>
+                                            <span>Endereço:<span className={styles.endereçoRua}>Rua Lima campos, 748</span></span>
+                                            <img src={endereçoMais} alt="maisopções" className={styles.imgEndereço} />
+                                        </div>
+                                        <button className={styles.pagarAgora}>Pagar agora</button>
+                                        {/* <img src={endereçoMais} alt="" /> */}
+                                </div>
+                            </div>
                         </div>
-                        <h2>Detalhes de Pagamento</h2>
-                        <div>Preço total</div>
-                        <div><span>Endereço :</span></div>
-                        <button>Pagar agora</button>
                     </div>
                 </main>
             </section>
